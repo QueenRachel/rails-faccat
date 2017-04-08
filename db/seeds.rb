233e2b224bbfbe33fb518ae137aaee7404d667f9
QueenRachel <raquelbrandao007@hotmail.com>
2017-04-08 15:39:55 -0300

    Adicionado Views de Admin e header

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,10 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+puts "=====> Criando Conta de Admin Básico"
+Admin.create!(email: 'admin@example.com', 
+			password: '123123123')
+
 puts "=====> Criando Páginas de exemplo"
 
 10.times do |index|
