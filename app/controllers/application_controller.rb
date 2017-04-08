c78b0cbf9cb3fc0863d5a8954895630f8a37494d
QueenRachel <raquelbrandao007@hotmail.com>
2017-04-08 17:48:45 -0300

    Adicionado as páginas ao header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :set_header_pages
+
+  	private
+
+  	def set_header_pages
+      @pages_header = Page.all
+    end
 end
